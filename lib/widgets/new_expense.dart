dd22159fcb8e34412408d6ecc148be003c7245bf
Ceren Yaşar <cerenysr1@gmail.com>
2025-07-09 23:04:53 +0300

    feat: create new_expense.dart

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
