df74bf92fd5bdbab8a719f57ea88cee20492d5e0
Ceren Yaşar <cerenysr1@gmail.com>
2025-07-09 23:28:33 +0300

    feat: add title input field to new expense form

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -4,8 +4,26 @@ class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: const [
+          TextField(
+            maxLength: 50,
+            decoration: InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
