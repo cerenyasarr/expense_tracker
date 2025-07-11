bc208b75254a02934c7ae37f4d45af5c9be9e58a
Ceren Yaşar <cerenysr1@gmail.com>
2025-07-11 23:25:58 +0300

    feat: enable selecting category and update state

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -15,6 +15,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -96,7 +97,12 @@ class _NewExpenseState extends State<NewExpense> {
                       )
                       .toList(),
                   onChanged: (value) {
-                    print(value);
+                    setState(() {
+                      if (value == null) {
+                        return;
+                      }
+                      _selectedCategory = value;
+                    });
                   }),
               TextButton(
                   onPressed: () {
