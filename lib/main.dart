1792a4a1909cb76e770ee2836c50768b55245e06
Ceren Yaşar <cerenysr1@gmail.com>
2025-07-15 21:49:46 +0300

    style: update scaffold background color with Material 3 theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: true),
+    theme: ThemeData().copyWith(
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 180, 135, 188)),
     home: const Expenses(),
   ));
 }
