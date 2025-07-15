3d26a1991a390f5a4f44a159f3398e1b21c5684c
Ceren Yaşar <cerenysr1@gmail.com>
2025-07-15 21:57:02 +0300

    fix: update theme with scaffoldBackgroundColor using ThemeData.light().copyWith

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData().copyWith(
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color.fromARGB(255, 180, 135, 188)),
-    home: const Expenses(),
-  ));
+  runApp(
+    MaterialApp(
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 235, 170, 208),
+      ),
+      home: const Expenses(),
+    ),
+  );
 }
