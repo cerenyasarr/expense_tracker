84e58d935306c14e52b982abbadb47bd48379581
Ceren Yaşar <cerenysr1@gmail.com>
2025-07-16 23:24:23 +0300

    style: lighten primary color in custom color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
-var kColorsScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple);
+var kColorsScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 124, 90, 182));
 
 void main() {
   runApp(
