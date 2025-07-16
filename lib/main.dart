b781c5d6b76fbc877b3d1ee5256993a0d45baf6b
Ceren Yaşar <cerenysr1@gmail.com>
2025-07-16 23:19:27 +0300

    style: apply custom color scheme using seedColor

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorsScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData.light().copyWith(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 235, 170, 208),
-      ),
+      theme: ThemeData.light().copyWith(),
       home: const Expenses(),
     ),
   );
