6ad56e2a359a5626bf246dec1aaa66e835126c2b
Ceren Yaşar <cerenysr1@gmail.com>
2025-07-20 00:23:38 +0300

    feat: add named constructor to filter expenses by category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -40,6 +40,11 @@ class ExpenseBucket {
     required this.expenses,
   });
 
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
   final Category category;
   final List<Expense> expenses;
 
@@ -49,8 +54,6 @@ class ExpenseBucket {
     for (final expense in expenses) {
       sum += expense.amount; // sum = sum + expense.amount
     }
-    return sum;
-
     return sum;
   }
 }
