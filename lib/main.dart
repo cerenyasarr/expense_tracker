2ba406539f3d996519d75557856e9c6ba7d3508f
Ceren Yaşar <cerenysr1@gmail.com>
2025-07-21 23:54:24 +0300

    chore: ensure widget binding before setting orientation

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ var kDarkColorScheme = ColorScheme.fromSeed(
     seedColor: const Color.fromARGB(255, 5, 99, 125));
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]).then((fn) {
